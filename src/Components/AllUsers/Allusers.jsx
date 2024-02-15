57a26c8afa84174c8ce80ed0963f1518e651da31
Anuj7815 <anujgupta08082001@gmail.com>
2024-02-15 09:38:24 +0530

    second comment

--- src/Components/AllUsers/Allusers.jsx
+++ src/Components/AllUsers/Allusers.jsx
@@ -5,35 +5,35 @@ import './Allusers.css'
 
 const Allusers = () => {
   return (
-     <div className="main-box">
-         
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-         <Users/>
-
-         
-
-        
-     </div>
+    <>
+      <div className="search-box">
+        <input type='text' placeholder='Search for a User' />
+      </div>
+      <div className="main-box">
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+        <Users />
+      </div>
+    </>
   )
 }
 
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Users.css";
-import pic from "../Users/download.jpeg";
-
+// import pic from "../Users/download.jpeg";
+import { Avatar } from '@mui/material'
 
 const Users = () => {
   return (
     <>
-      <div className="user-box">
+      {/* <div className="user-box">
         <div className="first-box">
           <img src={pic} alt="" />
           <div className="name">
@@ -29,6 +29,36 @@ const Users = () => {
           </div>
         </div>
         <button>Show More</button>
+      </div> */}
+
+
+      {/* ******************* New Code**************** */}
+
+      <div className="user-box">
+        <div className="first-section">
+          <div className="image-name-section">
+            <Avatar/>
+            <h2>UserName</h2>
+          </div>
+          <div className="button-section">
+            <button>Follow</button>
+            <button>Show More</button>
+          </div>
+        </div>
+        <div className="second-section">
+          <div className="column-box">
+            <h2>330</h2>
+            <h5>Post</h5>
+          </div>
+          <div className="column-box">
+            <h2>600</h2>
+            <h5>Followers</h5>
+          </div>
+          <div className="column-box">
+            <h2>20</h2>
+            <h5>Followings</h5>
+          </div>
+        </div>
       </div>
     </>
   );
